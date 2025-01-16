caacf9ae39e7681510b8b493317b95bcf280eb50
Your Juan Rueda <j.ruedavargas21@gmail.com>
2025-01-16 18:34:50 -0500

    Link Buttons and Create Calc Functions
    Create event listeners for specific buttons,
    Create display(), clear(), and operate() functions

--- script.js
+++ script.js
@@ -15,5 +15,47 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-
+    if (operator === '+') {
+        add(a, b);
+    }
+    if (operator === '-') {
+        subtract(a, b);
+    }
+    if (operator === 'x') {
+        multiply(a, b);
+    }
+    if (operator === '/') {
+        divide(a, b);
+    }
 }
+
+function clear() {
+    displayContent.textContent = '';
+}
+
+function display(n) {
+    displayContent.textContent += n;
+}
+
+
+const numberButton = document.querySelectorAll('.number');
+const operatorButton = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear')
+
+const displayContent = document.querySelector('.display');
+
+clearButton.addEventListener('click', clear);
+
+numberButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        numOne = event.target.textContent;
+        display(numOne);
+    });
+});
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        operator = event.target.textContent;
+        display(operator);
+    })
+})
