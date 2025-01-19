a6e33a9584bf22e10d5785093275f7b3f82a7724
Your Juan Rueda <j.ruedavargas21@gmail.com>
2025-01-19 16:18:35 -0500

    Adjust Logic in Event Listeners
    Properly concatenate and store values for operands,
    Adjust event listener to accept one operator at a time,
    Evaluate current expression if operator is clicked again

--- script.js
+++ script.js
@@ -11,51 +11,103 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        alert ('Are you dumb?');
+    }
+    else {
+        return a / b;
+    }
 }
 
 function operate(a, b, operator) {
+    const op1 = parseFloat(a);
+    const op2 = parseFloat(b);
+    let result;
+
     if (operator === '+') {
-        add(a, b);
+        result = add(op1, op2);
     }
     if (operator === '-') {
-        subtract(a, b);
+        result = subtract(op1, op2);
     }
     if (operator === 'x') {
-        multiply(a, b);
+        result = multiply(op1, op2);
     }
     if (operator === '/') {
-        divide(a, b);
+        result = divide(op1, op2);
     }
+    return result;
+}
+
+function del() {
+
 }
 
 function clear() {
     displayContent.textContent = '';
+    operandOne = '';
+    operandTwo = '';
+    operator = '';
 }
 
-function display(n) {
-    displayContent.textContent += n;
+function display(value) {
+    displayContent.textContent = value;
 }
 
 
-const numberButton = document.querySelectorAll('.number');
-const operatorButton = document.querySelectorAll('.operator');
-const clearButton = document.querySelector('.clear')
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const deleteButton = document.querySelector('.delete');
+const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
 
 const displayContent = document.querySelector('.display');
 
-clearButton.addEventListener('click', clear);
+let operandOne = '';
+let operandTwo = '';
+let operator = '';
 
-numberButton.forEach(button => {
+numberButtons.forEach(button => {
     button.addEventListener('click', (event) => {
-        numOne = event.target.textContent;
-        display(numOne);
+        const digit = event.target.textContent
+
+        if (operator === '') {
+            operandOne += digit;
+            display(operandOne);
+
+            console.log(operandOne);
+        }
+        else {
+            operandTwo += digit;
+            display(`${operandOne} ${operator} ${operandTwo}`);
+
+            console.log(operandTwo);
+        }
     });
 });
 
-operatorButton.forEach(button => {
+operatorButtons.forEach(button => {
     button.addEventListener('click', (event) => {
-        operator = event.target.textContent;
-        display(operator);
-    })
+        if (operandTwo !== '' && operator !== '') {
+            operandOne = operate(operandOne, operandTwo, operator);
+            operandTwo = '';
+            operator = event.target.textContent;
+            display(`${operandOne}`);
+        }
+        else {
+            operator = event.target.textContent;
+            display(`${operandOne} ${operator}`);
+        }
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    if (operandOne !== '' && operandTwo !== '' && operator !== '') {
+        operandOne = operate(operandOne, operandTwo, operator);
+        operandTwo = '';
+        operator = '';
+        display(operandOne);
+    }
 })
+
+clearButton.addEventListener('click', clear);
